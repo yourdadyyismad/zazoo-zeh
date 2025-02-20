5877fdf79ac139869e1a8ee4889ffba5e69bad81
yourdadyyismad <plsworm6@gmail.com>
2025-02-20 17:17:48 +0100

    Update P.js

--- P.js
+++ P.js
@@ -1,10 +1,12 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 
 (async () => {
     const browser = await puppeteer.launch({
         headless: true,
+        executablePath: '/usr/bin/chromium', // Make sure this points to the installed Chromium
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
+
     const page = await browser.newPage();
     await page.goto('https://example.com');
     console.log(await page.title());
