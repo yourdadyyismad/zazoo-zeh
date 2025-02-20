56dff302fb740af9ca140457a28ebf2ae08aad10
yourdadyyismad <plsworm6@gmail.com>
2025-02-20 07:42:35 +0100

    Create P.js

---
+++ P.js
@@ -0,0 +1,12 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+    console.log(await page.title());
+    await browser.close();
+})();
