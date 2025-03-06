ae1ef4329c3834afb23410dce8344c930586ea84
yourdadyyismad <plsworm6@gmail.com>
2025-03-06 03:03:31 +0100

    Update Movie.js

--- api/Movie.js
+++ api/Movie.js
@@ -1,5 +1,5 @@
     const express = require("express");
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-core");
 
 const router = express.Router();
 const BASE_URL = "https://www.letras.com";
